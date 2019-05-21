853a38ba7fc80e9563fc4cb1456efad01b55581a
Joshua Pierce <jpierce@myluckyday.fr>
2019-05-21 16:48:20 +0200

    initial commit

---
+++ FormationSwiftTests/FormationSwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  FormationSwiftTests.swift
+//  FormationSwiftTests
+//
+//  Created by MacBook on 5/21/19.
+//  Copyright © 2019 MyLuckyDay. All rights reserved.
+//
+
+import XCTest
+@testable import FormationSwift
+
+class FormationSwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
