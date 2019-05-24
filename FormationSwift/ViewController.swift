021ffdf3e957fd9a55a4e4a2b56f40d90277ed7d
Joshua Pierce <jpierce@myluckyday.fr>
2019-05-24 15:22:50 +0200

    added example

--- FormationSwift/ViewController.swift
+++ FormationSwift/ViewController.swift
@@ -13,8 +13,33 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        example()
+        exercise1()
     }
-
+    
+    private func example() {
+        // Create the view
+        let label = UILabel()
+        
+        // Configure the view
+        label.text = "Hello, World"
+        label.textColor = .blue
+        
+        // Set the layout
+        label.sizeToFit()
+        label.frame.origin = CGPoint(x: 50, y: 50)
+        
+        // Add to parent
+        view.addSubview(label)
+    }
+    
+    private func exercise1() {
+        // TODO: Add another label and a button
+        // When the button is tapped, change the text
+        
+        
+    }
 
 }
 
